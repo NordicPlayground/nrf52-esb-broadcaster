af804b7ce2f4f12772e5d7ddbba9ec5d98cad868
Torbjørn Øvrebekk <torbjorn.ovrebekk@nordicsemi.no>
2016-11-15 16:07:57 +0100

    Comments added.
    .gitignore and readme added

--- radio.h
+++ radio.h
@@ -4,10 +4,13 @@
 #include <stdint.h>
 #include <stdbool.h>
 
+// Definition of the data received callback, which is used to relay packets back to the application
 typedef void (*radio_packet_received_t)(uint8_t *packet, uint32_t packet_length);
 
+// Used to initialize the library, providing a network ID and a packet received callback
 uint32_t radio_init(uint16_t network_id, radio_packet_received_t packet_received_callback);
 
+// Function for sending packets to any receiver on the same network ID
 bool radio_packet_send(uint8_t *packet, uint32_t packet_length);
     
 #endif
